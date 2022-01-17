1ea3bc6462989d217ea083856fdb19733b676c40
Arijit Naskar <arijit.naskar89@gmail.com>
2022-01-17 18:45:34 +0530

    rubocop correction, age validator

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,12 +1,34 @@
-class Professor < ApplicationRecord
-    DESIGNATION_OPTIONS = %w(HOD Senior Junior Trainee)
+# frozen_string_literal: true
 
-    validates :name, presence: { message: "Name cannot be blank"}, length: {minimum: 3, maximum: 100, message: "Name should be greater than or equal 3 characters and smaller than or equal 100 characters"} , format: { with: /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/ , message: "Name should contain only letters." }
-    validates :email, presence: { message: "Email cannot be blank"}, uniqueness: {message: "This email is already taken"}
-    validates :mobile, format: { with: /\A\d+\z/, message: "Mobile can contain only numbers" }, length: {is: 10, message: "Mobile number should be of length 10 digits"}, allow_blank: true
-    validates :designation, presence: { message: "Designation cannot be blank"}, :inclusion => {:in => DESIGNATION_OPTIONS, message: "Designation can only be values from list HOD, Senior, Junior, Trainee"}
-    validates :joining_date, presence: { message: "Joining date cannot be blank"}, format: { with: /\A\d{2}\/\d{2}\/\d{4}\Z/, message: "Joining date should be of format dd/mm/YYYY" }, not_future: { message: "Joining date can't be in the future"}
-    validates :dob, presence: { message: "DOB cannot be blank"}, format: { with: /\d{2}\/\d{2}\/\d{4}/, message: "DOB should be of format dd/mm/YYYY" }, dob: { message: "Age should be greater than 18"}
-    validates :qualification, presence: { message: "Qualification cannot be blank"}
+class Professor < ApplicationRecord
+  DESIGNATION_OPTIONS = %w[HOD Senior Junior Trainee].freeze
+
+  validates :name, presence: { message: 'Name cannot be blank' },
+    length: {
+      minimum: 3,
+      maximum: 100,
+      message: 'Name should be greater than or equal 3 characters and smaller than or equal 100 characters' 
+    },
+    format: {
+        with: %r{/\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/},
+        message: 'Name should contain only letters.'
+    }
+  validates :email, presence: { message: 'Email cannot be blank' },
+    uniqueness: { message: 'This email is already taken' },
+    format: {
+        with: %r{/^(.+)@(.+)$/},
+        message: 'email should contain `@` symbol'
+    }
+  validates :mobile, format: { with: %r{/\A\d+\z/}, message: 'Mobile can contain only numbers' },
+    length: { is: 10, message: 'Mobile number should be of length 10 digits' }, allow_blank: true
+  validates :designation, presence: { message: 'Designation cannot be blank' },
+    inclusion: { in: DESIGNATION_OPTIONS, message: 'Designation can only be values from list HOD, Senior, Junior, Trainee' }
+  validates :joining_date, presence: { message: 'Joining date cannot be blank' },
+    format: { with: %r{/\A\d{2}\/\d{2}\/\d{4}\Z/}, message: 'Joining date should be of format dd/mm/YYYY' },
+    not_future: { message: 'Joining date can\'t be in the future' }
+  validates :dob, presence: { message: 'DOB cannot be blank' },
+    format: { with: %r{/\d{2}\/\d{2}\/\d{4}/}, message: 'DOB should be of format dd/mm/YYYY' },
+    dob: { message: 'Age should be greater than 18' }
+  validates :qualification, presence: { message: 'Qualification cannot be blank' }
 
 end
--- app/validators/dob_validator.rb
+++ app/validators/dob_validator.rb
@@ -4,7 +4,9 @@ class DobValidator < ActiveModel::EachValidator
             now = Time.now.utc.to_date
             birthday = value
             age = now.year - birthday.year - (birthday.to_date.change(:year => now.year) > now ? 1 : 0)
-            puts age
+            if age < 18
+                record.errors.add(attribute, options[:message] || "can't be less than 18 years")
+            end    
         end
     end
 end
