541871c265f66fa88fd4e41c7cd5f803ca7aeee2
Arijit Naskar <arijit.naskar89@gmail.com>
2022-01-20 17:21:01 +0530

    rspec for professor

--- spec/requests/api/v1/professors_spec.rb
+++ spec/requests/api/v1/professors_spec.rb
@@ -164,5 +164,153 @@ RSpec.describe "Api::V1::Professors", type: :request do
       end
     end
 
+    context 'For email uniqueness check failed' do
+      before do
+        existed_user =  Professor.find_by(email: "dodo@gmail.com")
+        post '/api/v1/professors', params: request_params, as: :json
+      end
+
+      it "should return failure status" do
+        expect(response).to have_http_status(422)
+      end
+
+      it "should have failure message" do
+        json_response = JSON.parse(response.body)
+        expect(json_response['message']).to eq("Failed To Create Professor")
+      end
+
+      it "should contain errors" do
+        json_response = JSON.parse(response.body)
+        expect(json_response['errors']['email'][0]).to eq('email should contain `@` symbol and valid domain after `@`')
+      end
+    end
+
+    context 'For designation presence check failed' do
+      before do
+        request_params[:designation] = nil
+        post '/api/v1/professors', params: request_params, as: :json
+      end
+
+      it "should return failure status" do
+        expect(response).to have_http_status(422)
+      end
+
+      it "should have failure message" do
+        json_response = JSON.parse(response.body)
+        expect(json_response['message']).to eq("Failed To Create Professor")
+      end
+
+      it "should contain errors" do
+        json_response = JSON.parse(response.body)
+        expect(json_response['errors']['designation'][0]).to eq('Designation cannot be blank')
+      end
+    end
+
+    context 'For designation in acceptable values check failed' do
+      before do
+        request_params[:designation] = 'test'
+        post '/api/v1/professors', params: request_params, as: :json
+      end
+
+      it "should return failure status" do
+        expect(response).to have_http_status(422)
+      end
+
+      it "should have failure message" do
+        json_response = JSON.parse(response.body)
+        expect(json_response['message']).to eq("Failed To Create Professor")
+      end
+
+      it "should contain errors" do
+        json_response = JSON.parse(response.body)
+        expect(json_response['errors']['designation'][0]).to eq('Designation can only be values from list HOD, Senior, Junior, Trainee')
+      end
+    end
+
+    context 'For Joining date presence check failed' do
+      before do
+        request_params[:joining_date] = nil
+        post '/api/v1/professors', params: request_params, as: :json
+      end
+
+      it "should return failure status" do
+        expect(response).to have_http_status(422)
+      end
+
+      it "should have failure message" do
+        json_response = JSON.parse(response.body)
+        expect(json_response['message']).to eq("Failed To Create Professor")
+      end
+
+      it "should contain errors" do
+        json_response = JSON.parse(response.body)
+        expect(json_response['errors']['joining_date'][0]).to eq('Joining date cannot be blank')
+      end
+    end
+
+    context 'For Joining date proper date format check failed' do
+      before do
+        request_params[:joining_date] = '11/22/2111'
+        puts request_params
+        post '/api/v1/professors', params: request_params, as: :json
+      end
+
+      it "should return failure status" do
+        expect(response).to have_http_status(422)
+      end
+
+      it "should have failure message" do
+        json_response = JSON.parse(response.body)
+        expect(json_response['message']).to eq("Failed To Create Professor")
+      end
+
+      it "should contain errors" do
+        json_response = JSON.parse(response.body)
+        expect(json_response['errors']['joining_date'][0]).to eq('Joining date should be of format dd/mm/YYYY')
+      end
+    end
+
+    context 'For Joining date valid date check failed' do
+      before do
+        request_params[:joining_date] = '21/01/2022'
+        post '/api/v1/professors', params: request_params, as: :json
+      end
+
+      it "should return failure status" do
+        expect(response).to have_http_status(422)
+      end
+
+      it "should have failure message" do
+        json_response = JSON.parse(response.body)
+        expect(json_response['message']).to eq("Failed To Create Professor")
+      end
+
+      it "should contain errors" do
+        json_response = JSON.parse(response.body)
+        expect(json_response['errors']['joining_date'][0]).to eq('Joining date can\'t be in the future')
+      end
+    end
+
+    context 'For dob presence check failed' do
+      before do
+        request_params[:dob] = nil
+        post '/api/v1/professors', params: request_params, as: :json
+      end
+
+      it "should return failure status" do
+        expect(response).to have_http_status(422)
+      end
+
+      it "should have failure message" do
+        json_response = JSON.parse(response.body)
+        expect(json_response['message']).to eq("Failed To Create Professor")
+      end
+
+      it "should contain errors" do
+        json_response = JSON.parse(response.body)
+        expect(json_response['errors']['dob'][0]).to eq('DOB cannot be blank')
+      end
+    end
+
   end
 end
